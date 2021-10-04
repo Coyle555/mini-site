e0a7e8fc95db53e4449d7a7730d49e16a4158a12
Carsons Mac <carsoncoyle2015@gmail.com>
2021-10-04 19:38:21 -0400

    tweaked server files to serve react app

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import bodyParser from 'body-parser';
 
+
 const PORT = process.env.PORT || 3002;
 
 const app = express();
@@ -8,6 +9,13 @@ const app = express();
 app.use(bodyParser.json())
 
 const users = []
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 
 app.get("/api", (req, res) => {
