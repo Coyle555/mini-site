ab0f04745c32e11a9d54e444c430d256f5705d8d
Carsons Mac <carsoncoyle2015@gmail.com>
2021-10-04 19:52:16 -0400

    more server tweaking for proper app function

--- server/server.js
+++ server/server.js
@@ -1,5 +1,11 @@
 import express from 'express'
 import bodyParser from 'body-parser';
+import path from 'path'
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 
 const PORT = process.env.PORT || 3002;
